5d3167b416c7929962c5e3442de1df2d1218d9be
Axl <axel.coenon@gmail.com>
2019-01-14 15:03:42 +0100

    2eme

--- src/main/java/fr/formation/proxi4/metier/Customer.java
+++ src/main/java/fr/formation/proxi4/metier/Customer.java
@@ -1,5 +1,84 @@
 package fr.formation.proxi4.metier;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	private Integer id;
+	
+	@Column
+	private String firstName;
+	
+	@Column
+	private String lastName;
+	
+	@Column
+	private String phone;
+	
+	@Column
+	private String mail;
+	
+	@Column
+	private String clientNumber;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getClientNumber() {
+		return clientNumber;
+	}
+
+	public void setClientNumber(String clientNumber) {
+		this.clientNumber = clientNumber;
+	}
+	
+	
+	
 }
--- src/main/java/fr/formation/proxi4/metier/Opinion.java
+++ src/main/java/fr/formation/proxi4/metier/Opinion.java
@@ -1,5 +1,74 @@
 package fr.formation.proxi4.metier;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="opinion")
 public class Opinion {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	private Integer id;
+	
+	@Column
+	private Boolean isPositive;
+	
+	@Column
+	private String comment;
+	
+	@Column
+	private Customer customer;
+	
+	@Column
+	private Survey survey;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Boolean getIsPositive() {
+		return isPositive;
+	}
+
+	public void setIsPositive(Boolean isPositive) {
+		this.isPositive = isPositive;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Survey getSurvey() {
+		return survey;
+	}
+
+	public void setSurvey(Survey survey) {
+		this.survey = survey;
+	}
+	
+	
+	
 }
